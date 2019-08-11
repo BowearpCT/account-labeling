18df2685ff06834745ad1b211b79dfb5f08441ef
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-08-11 20:39:44 +0700

    component form assignment

--- app/src/components/FormAssign.vue
+++ app/src/components/FormAssign.vue
@@ -7,35 +7,50 @@
     </b-row>
     <hr />
     <b-form @submit.stop.prevent>
-      <b-row>
+      <b-row align-h="start">
         <b-col md="3">
-          <b-form-group id="fieldset-horizontal" label="Assign to :" label-for="input-username"></b-form-group>
+          <b-form-group label="Assign to :"></b-form-group>
         </b-col>
-        <b-col md="5">
-          <b-form-select v-model="selected" :options="options"></b-form-select>
+        <b-col md="3">
+          <b-form-select id="input-username" v-model="selected" :options="options"></b-form-select>
         </b-col>
-        <b-col md="4">
-          <b-form-select v-model="numberOfAccout" class="mb-3">
-            <option value="null">number of account</option>
-            <option value="100">100</option>
-            <option value="200">200</option>
-          </b-form-select>
+      </b-row>
+      <br />
+      <b-row align-h="start">
+        <b-col md="3">
+          <b-form-group label="Category :"></b-form-group>
+        </b-col>
+        <b-col md="9">
+          <b-form-radio-group id="radio-group-2" v-model="category" name="radio-sub-component">
+            <b-form-radio value="first">Toggle this custom radio</b-form-radio>
+            <b-form-radio value="second">Or toggle this other custom radio</b-form-radio>
+            <b-form-radio value="third">This one is Disabled</b-form-radio>
+            <b-form-radio :value="{ fourth: 4 }">This is the 4th radio</b-form-radio>
+          </b-form-radio-group>
         </b-col>
       </b-row>
       <br />
       <b-row>
         <b-col md="3">
-          <b-form-group id="fieldset-horizontal" label="Category :" label-for="input-horizontal"></b-form-group>
+          <b-form-group
+            id="fieldset-horizontal"
+            label="numbers account :"
+            label-for="input-username"
+          ></b-form-group>
         </b-col>
-        <b-col md="8">
-          <b-form-group>
-            <b-form-checkbox-group
-              id="checkbox-group-1"
-              v-model="selected"
-              :options="options"
-              name="flavour-1"
-            ></b-form-checkbox-group>
-          </b-form-group>
+        <b-col md="2">
+          <b-input v-model="numberOfAccout"></b-input>
+        </b-col>
+        <b-col md="3">
+          <b-form-group id="fieldset-horizontal" label="channel :" label-for="input-username"></b-form-group>
+        </b-col>
+        <b-col md="2">
+          <b-form-select v-model="channel" :options="options">
+            <option :value="null" disabled>select</option>
+            <option value="a">twitter</option>
+            <option value="b">instagram</option>
+            <option value="c">facebook</option>
+          </b-form-select>
         </b-col>
       </b-row>
       <br />
@@ -44,17 +59,19 @@
           <b-form-group id="fieldset-horizontal" label="Filter :" label-for="input-horizontal"></b-form-group>
         </b-col>
         <b-col md="5">
-          <b-form-input></b-form-input>
+          <b-form-input placeholder="what things your filter"></b-form-input>
         </b-col>
       </b-row>
+      <br />
       <b-row>
         <b-col>
           <b-form-group></b-form-group>
         </b-col>
       </b-row>
+      <br />
       <b-row>
-        <b-col md="9">
-          <b-button type="submit" size="md" variant="danger">submit</b-button>
+        <b-col md="12">
+          <b-button type="submit" size="lg" variant="danger">submit</b-button>
         </b-col>
       </b-row>
     </b-form>
@@ -65,15 +82,18 @@
 export default {
   data() {
     return {
-      selected: [],
-      options: [
-        { value: null, text: "Type of profile" },
-        { value: "a", text: "Interest" },
-        { value: "a", text: "Interest" },
-        { value: "a", text: "Interest" },
-        { value: "a", text: "Interest" }
-      ],
-      numberOfAccout: ""
+      // selected: [],
+      // options: [
+      //   { value: null, text: "Type of profile" },
+      //   { value: "a", text: "Interest" },
+      //   { value: "a", text: "Interest" },
+      //   { value: "a", text: "Interest" },
+      //   { value: "a", text: "Interest" }
+      // ],
+      numberOfAccout: "100",
+      assignTo: "",
+      channel: null,
+      category: "first"
     };
   },
   computed: {},
