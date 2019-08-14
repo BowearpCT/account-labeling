6a1f92270babaf170328de8a81e28c5d87590222
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-08-14 15:33:07 +0700

    create label that is category route

--- routes/label.js
+++ routes/label.js
@@ -1,26 +1,31 @@
-var express = require('express')
-var router = express.Router()
-const accountModel = require("../model/account-model")
-const labelModel = require("../model/label-model")
-const userModel = require("../model/user-model")
-const labelingModel = require("../model/labeling-model")
+var express = require('express');
+var router = express.Router();
+const { getLabelsThatIsCategory } = require("../helper/query");
 
 // all label
-router.get("/label",(req, res) => {
-    accountModel.findAll({
-        where:{
-            "$labelings.account_id$" :null 
-        },
-        include:[{
-            model : labelingModel
-        }]
-    })
-    .then(accounts => {
-        res.json(accounts)
-    })
-    .catch(err => {         
-        res.send("error :"+ err)
-    })
-})
+// router.get("/label", (req, res) => {
+//   accountModel.findAll({
+//     where: {
+//       "$labelings.account_id$": null
+//     },
+//     include: [{
+//       model: labelingModel
+//     }]
+//   })
+//     .then(accounts => {
+//       res.json(accounts)
+//     })
+//     .catch(err => {
+//       res.send("error :" + err)
+//     })
+// })
 
+router.get("/label/category", async (req, res) => {
+  try {
+    const categories = await getLabelsThatIsCategory();
+    res.send(categories);
+  } catch (error) {
+    res.send(error);
+  }
+})
 module.exports = router
