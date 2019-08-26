4555b38ce3f197078af78b2ae6d49a9f8f9d087a
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-08-26 16:24:00 +0700

    add component FacebookEmbed and instagramEmbed

---
+++ app/src/components/FacebookEmbed.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="fb-page"
+    data-tabs="timeline"
+    data-href="https://www.facebook.com/sompondofficial"
+    data-width="380"
+    data-hide-cover="false"
+  ></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
---
+++ app/src/components/InstagramEmbed.vue
@@ -0,0 +1,12 @@
+<template>
+  <a class="twitter-timeline" href="https://twitter.com/UnderbedDara"></a>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
