2e194d6e3d882017a46e654b7a52af30b4ab6c82
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-08-28 14:13:42 +0700

    add route find descendents by id

--- routes/label.js
+++ routes/label.js
@@ -3,7 +3,8 @@ var router = express.Router();
 const {
   getLabelsThatIsCategory,
   findAncestorLabels,
-  findDescendentLabels
+  findDescendentLabels,
+  findLabelByName
 } = require("../helper/query");
 
 router.get("/labels", (req, res) => {
@@ -23,18 +24,29 @@ router.get("/labels", (req, res) => {
     })
 })
 
-router.get("/label/descendents/:labelName", async (req, res) => {
+router.get("/label/descendents/id/:labelId", async (req, res) => {
   try {
-    const labels = await findDescendentLabels(req.params.labelName);
+    const labels = await findDescendentLabels(req.params.labelId);
     res.send(labels)
   } catch (error) {
     res.send(error)
   }
 })
 
-router.get("/label/ancestors/:labelName", async (req, res) => {
+router.get("/label/descendents/name/:labelName", async (req, res) => {
   try {
-    const labels = await findAncestorLabels(req.params.labelName);
+    const label = await findLabelByName(req.params.labelName)
+    const labels = await findDescendentLabels(label.id);
+    res.send(labels)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+
+router.get("/label/ancestors/:parentId", async (req, res) => {
+  try {
+    const labels = await findAncestorLabels(req.params.parentId);
     res.send(labels)
   } catch (error) {
     res.send(error)
