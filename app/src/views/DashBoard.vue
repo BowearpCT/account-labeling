59329b004d416205bfd0211d40f9944cb6a8ac76
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-09-03 17:06:29 +0700

    add input selection and assignment button in dash board

--- app/src/views/DashBoard.vue
+++ app/src/views/DashBoard.vue
@@ -10,9 +10,36 @@
           <b-row>
             <h3>Dashboard</h3>
           </b-row>
+          <b-row>
+            <b-col md="2">
+              <multiselect
+                @select="userAssignments"
+                v-model="value"
+                deselect-label="Can't remove this value"
+                track-by="name"
+                label="name"
+                placeholder="Select user"
+                :options="users"
+                :searchable="false"
+                :allow-empty="false"
+              ></multiselect>
+            </b-col>
+            <b-col md="2">
+              <b-col lg="4" class="pb-2">
+                <b-button variant="outline-info" @click="goAssignment">add assignment</b-button>
+              </b-col>
+            </b-col>
+          </b-row>
           <hr />
           <b-row v-show="assignments">
-            <b-col v-for="(assignment, index) in assignments" :key="index" sm="12" md="6" xl="4">
+            <b-col
+              v-for="(assignment, index) in assignments"
+              :key="index"
+              sm="12"
+              md="6"
+              lg="4"
+              xl="4"
+            >
               <Progress v-bind:assignment="{assignment}"></Progress>
             </b-col>
           </b-row>
@@ -26,13 +53,17 @@
 // @ is an alias to /src
 import Nav from "@/components/Nav.vue";
 import Progress from "@/components/Progress.vue";
+import Multiselect from "vue-multiselect";
 export default {
   data() {
-    return {};
+    return {
+      value: {}
+    };
   },
   components: {
     Nav,
-    Progress
+    Progress,
+    Multiselect
   },
   computed: {
     assignments() {
@@ -40,14 +71,29 @@ export default {
     },
     user() {
       return this.$store.getters.user;
+    },
+    users() {
+      return this.$store.getters.users;
     }
   },
   created() {
+    this.getusers();
     this.getAssignments();
   },
   methods: {
     getAssignments() {
       this.$store.dispatch("assignments");
+    },
+    getusers() {
+      this.$store.dispatch("fetcherUsers");
+    },
+    goAssignment() {
+      this.$router.push("/assign");
+    },
+    async userAssignments() {
+      setTimeout(() => {
+        this.$store.dispatch("assignments", this.value.id);
+      }, 100);
     }
   }
 };
