41fada69820487f77270bdcb72203a28bf588789
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-09-05 14:54:27 +0700

    add dash board user component

--- app/src/views/DashBoardUser.vue
+++ app/src/views/DashBoardUser.vue
@@ -1,26 +1,72 @@
-
 <template>
-  <div class="about">
+  <div class="assign">
     <Nav />
-    <h1>This is an user page</h1>
-    <b-button @click="goLabelling">labelling</b-button>
+    <br />
+    <br />
+    <br />
+    <b-container fluid>
+      <b-row>
+        <b-col offset-md="2" md="8">
+          <b-row>
+            <h3>Dashboard</h3>
+          </b-row>
+          <hr />
+          <b-row v-show="assignments">
+            <b-col
+              v-for="(assignment, index) in assignments"
+              :key="index"
+              sm="12"
+              md="6"
+              lg="4"
+              xl="4"
+            >
+              <Progress v-bind:assignment="{assignment}"></Progress>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Nav from "../components/Nav";
+// @ is an alias to /src
+import Nav from "@/components/Nav.vue";
+import Progress from "@/components/Progress.vue";
+import Multiselect from "vue-multiselect";
 export default {
+  data() {
+    return {
+      value: {},
+      options: []
+    };
+  },
   components: {
-    Nav
+    Nav,
+    Progress,
+    Multiselect
+  },
+  computed: {
+    assignments() {
+      return this.$store.getters.assignments;
+    },
+    user() {
+      return this.$store.getters.user;
+    }
+  },
+  created() {
+    this.getAssignments();
   },
   methods: {
-    async goLabelling() {
-      await this.$store.dispatch("labels", { category: "type of profile" });
+    getAssignments() {
+      this.$store.dispatch("assignments", this.user.id);
+    },
+    userAssignments() {
       setTimeout(() => {
-        this.$router.push("/labelling");
-        location.reload();
-      }, 1000);
+        this.$store.dispatch("assignments", this.value.id);
+      }, 100);
     }
   }
 };
 </script>
+
