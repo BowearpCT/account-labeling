9c473ebcdd40a55be5bccee7d90370c694c382a3
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-09-10 19:11:10 +0700

    fix bug

--- app/src/auth/user.js
+++ app/src/auth/user.js
@@ -1,7 +1,7 @@
 import { store } from "../store/index.js";
 
 export default (to, from, next) => {
-  var user = store.getters.user;
+  const user = store.getters.user;
   if (user) {
     if (user.role == "2") {
       next();
