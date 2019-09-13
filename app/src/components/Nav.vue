705e6514887d05948904a8570b616c70ca39de0d
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-09-13 14:55:16 +0700

    add link to dashboard and search page

--- app/src/components/Nav.vue
+++ app/src/components/Nav.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
     <b-navbar toggleable="lg">
-      <b-navbar-brand style="color:white;" href="#" @click="redirectDash">Source Labeling</b-navbar-brand>
-
+      <b-navbar-brand style="color:white;" href="#" @click="redirectDash"><b>Source Labelling</b></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
       <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a @click="goDashboard" style="color:white;" class="nav-link" href="#">Dashboard<span class="sr-only">(current)</span></a>
+            </li>
+            <li v-show="user.role==1" class="nav-item">
+              <a @click="goSearch" style="color:white;" class="nav-link" href="#">Search</a>
+            </li>
+          </ul>
+        </b-navbar-nav> 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template slot="button-content">
-              <em style="color:white;">{{ user.username }}</em>
+              <em ><b>{{ user.username }}</b></em>
             </template>
-            <b-dropdown-item style="color:white;" href="#">Profile</b-dropdown-item>
             <b-dropdown-item @click="signout">logout</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
@@ -24,6 +31,10 @@
 <script>
 export default {
   name: "NavBar",
+  data(){
+    return {
+    }
+  },
   computed: {
     user() {
       return this.$store.getters.user;
@@ -40,7 +51,14 @@ export default {
         this.$store.commit("clearLabels");
         this.$router.push("/dashboard");
       }, 500);
+    },
+    goSearch(){
+      this.$router.push("/search")
+    },
+    goDashboard(){
+      this.$router.push("/dashboard")
     }
+    
   }
 };
 </script>
@@ -49,5 +67,8 @@ export default {
 .navbar {
   background-color: #101820;
   color: white;
+}
+em{
+  color:white;
 }
 </style>
