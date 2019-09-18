302e266b54641863e73773110dde0b8c212d469c
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-09-18 19:12:42 +0700

    add route to get account labellings and  filter it

--- api/routes/account.js
+++ api/routes/account.js
@@ -1,10 +1,21 @@
 var express = require('express')
 var router = express.Router()
-const {formatAccountLabellings} = require("../helper/formater")
+const {
+  checkFilter
+} = require("../helper/checker")
+const {
+  formatAccountLabellings,
+  formatCategoriesLabelsId,
+  formatAccountsId
+} = require("../helper/formater")
 const {
   findAccountBooking,
   findAccountLabelling,
-  insertLabellings
+  insertLabellings,
+  updateReservedAccount,
+  findAccountsLabelling,
+  findAccountsLabelsByAccountsId,
+  filterAccounts
 } = require("../helper/query");
 
 router.get("/account/booking/:assignmentId", async (req, res) => {
@@ -20,6 +31,7 @@ router.post("/account/labelling/", async (req, res) => {
   try {
     const accountlabellings = await formatAccountLabellings(req.body.accountReservedId, req.body.labels);
     const labellingResult = await insertLabellings(accountlabellings);
+    await updateReservedAccount(req.body.accountReservedId);
     res.send(labellingResult)
   } catch (error) {
     res.send(error)
@@ -30,6 +42,41 @@ router.get("/account/labelling/:accountReservedId",async (req, res) => {
   try {
     const labels = await findAccountLabelling(req.params.accountReservedId);
     res.send(labels)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.get("/accounts/labelling",async (req,res) => {
+  try {
+    const accounts = await findAccountsLabelling();
+    res.send(accounts);
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.get("/accounts/labelling/filter",async (req, res) => {
+  const categoryLabels = JSON.parse(req.query.labels)
+  const categoriesLabelsId = await formatCategoriesLabelsId(categoryLabels)
+  const labelsId = Object.values(categoriesLabelsId)
+  console.log(labelsId)
+  let filter
+  let accountLabels 
+  try {
+    filter = await checkFilter(labelsId)
+    console.log(filter)
+    if(filter){
+      accounts = await filterAccounts(categoriesLabelsId)
+      accountsId = await formatAccountsId(accounts)
+      accountLabels = await findAccountsLabelsByAccountsId(accountsId)
+      console.log("debug",accountLabels)
+    }
+    else{
+      accountLabels = await findAccountsLabelling()
+    }
+    console.log(accountLabels)
+    res.send(accountLabels)
   } catch (error) {
     res.send(error)
   }
