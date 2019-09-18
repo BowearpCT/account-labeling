ef8c16e79278cf84e1b0c7a0463bcd1a49671926
BowearpCT <c.kunapakornkarn@hotmail.com>
2019-09-18 19:16:03 +0700

    add func fetch acc filter when variable change

--- app/src/views/Search.vue
+++ app/src/views/Search.vue
@@ -64,12 +64,12 @@
             </b-col>
             <b-col cols="3">
               <label ><b>{{categoryLabels[2].name}}</b></label>
-              <multiselect v-model="filter.interest"  deselect-label="Can't remove this value" track-by="name" label="name" placeholder="select labels" :multiple="true" :options="categoryLabels[2].labels" :searchable="true" :allow-empty="true">
+              <multiselect @remove="fetchAccountsFilter" v-model="filter.interest"  deselect-label="Can't remove this value" track-by="name" label="name" placeholder="select labels" :multiple="true" :options="categoryLabels[2].labels" :searchable="true" :allow-empty="true">
               </multiselect>
             </b-col>
             <b-col cols="3">
               <label ><b>{{categoryLabels[3].name}}</b></label>
-              <multiselect v-model="filter.demographicOrTarget"  deselect-label="Can't remove this value" track-by="name" label="name" placeholder="select labels" :multiple="true" :options="categoryLabels[3].labels" :searchable="true" :allow-empty="true">
+              <multiselect @remove="fetchAccountsFilter" v-model="filter.demographicOrTarget"  deselect-label="Can't remove this value" track-by="name" label="name" placeholder="select labels" :multiple="true" :options="categoryLabels[3].labels" :searchable="true" :allow-empty="true">
               </multiselect>
             </b-col>
           </b-row>
@@ -78,7 +78,7 @@
       </b-row>
       <b-row align-h="center">
         <b-col cols="10">
-          <AccountsTable />
+          <AccountsTable :channel="channel" :like="likedSelected"/>
         </b-col>
       </b-row>
     </b-container>
@@ -101,15 +101,15 @@ export default {
         demographicOrTarget:[]
       },
       likedCount: [
-        { text: "all", value: "" },
-        { text: "> 10,000", value: "10000" },
-        { text: "> 50,000", value: "50000" },
-        { text: "> 100,000", value: "100000" },
-        { text: "> 200,000", value: "200000" },
-        { text: "> 500,000", value: "500000" },
-        { text: "> 1,000,000", value: "1000000" },
-        { text: "> 5,000,000", value: "5000000" },
-        { text: "> 10,000,000", value: "10000000" },
+        { text: "all", value: 0 },
+        { text: "> 10,000", value: 10000 },
+        { text: "> 50,000", value: 50000 },
+        { text: "> 100,000", value: 100000 },
+        { text: "> 200,000", value: 200000 },
+        { text: "> 500,000", value: 500000 },
+        { text: "> 1,000,000", value: 1000000 },
+        { text: "> 5,000,000", value: 5000000 },
+        { text: "> 10,000,000", value: 10000000 },
       ],
       channels: [
         { text: "all", value: "" },
@@ -124,8 +124,9 @@ export default {
       ],
       channel:"",
       selected:null,
-      likedSelected:"",
-      search:""
+      likedSelected:0,
+      search:"",
+      selectedLabels: []
     }
   },
   watch:{
@@ -133,7 +134,16 @@ export default {
       console.log("fetching")
     },
     typeOfProfile(){
-      console.log(this.categoryLabels)
+      this.fetchAccountsFilter()
+    },
+    topicByBusiness(){
+      this.fetchAccountsFilter()
+    },
+    interest(){
+      this.fetchAccountsFilter()
+    },
+    demographicOrTarget(){
+      this.fetchAccountsFilter()
     }
   },
   components :{
@@ -146,22 +156,33 @@ export default {
       return this.$store.getters.categoryLabels;
     },
     typeOfProfile(){
-      return this.filter.typeOfProfile
+      return this.filter.typeOfProfile;
     },
     topicByBusiness(){
-      return this.filter.topicByBusiness
+      return this.filter.topicByBusiness;
     },
     interest(){
-      return this.filter.interest
+      return this.filter.interest;
     },
     demographicOrTarget(){
-      return this.filter.demographicOrTarget
-    }
+      return this.filter.demographicOrTarget;
+    } 
   },
   created(){
     this.fetchCategoryLabels();
   },
   methods:{
+    fetchAccountsFilter(){
+      this.selectedLabels = [...this.typeOfProfile, ...this.topicByBusiness, ...this.interest, ...this.demographicOrTarget]
+      const payload = {
+        typeOfProfile : this.typeOfProfile,
+        topicByBusiness : this.topicByBusiness,
+        interest : this.interest,
+        demographicOrTarget : this.demographicOrTarget
+      }
+      console.log(payload)
+      this.$store.dispatch("fetchAccountsFilter", payload);
+    },
     fetchCategoryLabels() {
       this.$store.dispatch("fetchLabelsCategory")
     }
